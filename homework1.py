9690f30084a16c0fe75e22bd440f177aa30c6f19
dongh104 <151799042+dongh104@users.noreply.github.com>
2023-11-26 20:01:24 +0900

    homework1.py

---
+++ homework1.py
@@ -0,0 +1,6 @@
+one=int(input("첫번째 값 입력:")) 
+two=int(input("두번째 값 입력:")) 
+three=int(input("세번째 값 입력:"))
+Sum=(one+two+three)
+avg=(one+two+three)/3 
+print(one, two, three,"입력된값의 합은 ",Sum, "이고 평균값은 ",avg,"입니다.")
